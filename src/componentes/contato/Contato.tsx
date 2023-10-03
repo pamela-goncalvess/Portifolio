906d7625fcad0d772e88b26c405f2d3009122d2e
pamela-goncalvess <pamela.goncalves@estudante.ifms.edu.br>
2023-10-03 11:01:13 -0400

    atualizacao 2- 03/10

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,11 +1,33 @@
 import './Contato.css'
-export default function Contato(){
+import '../header/Header'
+
+ function Contato(){
     return(
-        <div className='content-main'>Página de Contato!</div>
+       <>
+            <form action="tela-contato">
+                <div>
+                    <label htmlFor="primeiro-nome">Primeiro Nome:</label>
+                    <input type="texto" id="primeiro-nome" name="primeiro-nome-usuario"></input>
+                </div>
+                <div>
+                    <label htmlFor="ultimo-nome">Último Nome:</label>
+                    <input type="texto" id="ultimo-nome" name="ultimo-nome-usuario"></input>
+                </div>
+                <div>
+                    <label htmlFor="email">E-mail:</label>
+                    <input type="email" id="email" name="email-usuario"></input>
+                </div>
+                <div>
+                    <label htmlFor="mensagem">Digite sua mensagem:</label>
+                    <textarea  id="texto" name="mensagem-usuario"></textarea>
+                </div>
+                    <div className="botao-enviar">
+                    <button type="submit">Enviar Mensagem</button>
+                </div>
+            </form>
+
+       </>
     )
 }
-    <div className='infos-contato'>
-            <p>Primeiro Nome</p>
-            <p>Último Nome</p>
-    </div>   
+export default Contato      
 
