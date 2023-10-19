5d0046481347fec00ab57af96822aa4f9309a871
pamela-goncalvess <pamela.goncalves@estudante.ifms.edu.br>
2023-10-19 10:15:50 -0400

    commit semi-final

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -1,7 +1,24 @@
 import './Contato.css'
 import '../header/Header'
+import {useState} from "react"
 
  function Contato(){
+    const [nome, setNome] = useState("")  
+  const [email, setEmail] = useState("")
+  const [mensagem, setMensagem] = useState("")
+  function mudaNome(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setNome(e.target.value)
+  }
+  function mudaEmail(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setEmail(e.target.value)
+  }
+  function mudaMensagem(e:React.ChangeEvent<HTMLInputElement>){
+    console.log(e.target.value)
+    setMensagem(e.target.value)
+  }
+
     return(
        <>
        <div className='titulo-contato'>
@@ -9,20 +26,16 @@ import '../header/Header'
        </div>
         <form className="tela-contato">
                 <div>
-                    <label htmlFor="primeiro-nome">Primeiro Nome:</label>
-                    <input type="texto" id="primeiro-nome" name="primeiro-nome-usuario"></input>
+                    <label htmlFor="nome">Nome: {nome}</label>
+                    <input type="texto" id="nome" name="nome-usuario" onChange={mudaNome}></input>
                 </div>
                 <div>
-                    <label htmlFor="ultimo-nome">Último Nome:</label>
-                    <input type="texto" id="ultimo-nome" name="ultimo-nome-usuario"></input>
+                    <label htmlFor="email">Email:{email}</label>
+                    <input type="email" id="email" name="email-usuario"  onChange={mudaEmail}></input>
                 </div>
                 <div>
-                    <label htmlFor="email">E-mail:</label>
-                    <input type="email" id="email" name="email-usuario"></input>
-                </div>
-                <div>
-                    <label htmlFor="mensagem">Digite sua mensagem:</label>
-                    <textarea  id="texto" name="mensagem-usuario"></textarea>
+                    <label htmlFor="mensagem">Digite sua mensagem: {mensagem} </label>
+                    <input  id="texto" name="mensagem-usuario" onChange={mudaMensagem}></input>
                 </div>
                 <div >
                     <button className="botao" type="submit">Enviar Mensagem</button>
